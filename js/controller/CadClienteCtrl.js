8e26f64b382e850464a5b132e047767ae9a4aecb
Ricardo Almendro Ruiz <ricardo.almendro.ruiz@gmail.com>
2016-05-15 20:56:16 -0300

    Progresso Aula 12

---
+++ js/controller/CadClienteCtrl.js
@@ -0,0 +1,13 @@
+app.controller("CadClienteCtrl", function($scope){
+    $scope.app="Cadastro de Clientes";
+    
+    $scope.clientes = [
+        {nome : "Ricardo Ruiz",codigo : "0001"},
+        {nome : "Cinthya Hayane de Carvalhor",codigo : "0002"},
+        {nome : "Guilherme Carvalho Ruiz",codigo : "0003"}
+    ];
+    
+    $scope.adicionarClientes = function(cliente){
+        $scope.clientes.push(angular.copy(cliente));
+    };
+})
